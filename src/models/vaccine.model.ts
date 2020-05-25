f2606876ff8ca36cd0b4198a83300754f3acadd7
Hérlon Manollo C Guedes <manollo.guedes11@gmail.com>
2020-05-25 18:53:49 -0300

    feat: creating vaccina's validate and getAcceptableStructure methods

--- src/models/vaccine.model.ts
+++ src/models/vaccine.model.ts
@@ -9,4 +9,20 @@ export class VaccineModel extends MongoSchema {
     this._id = id;
     this.name = name;
   }
+
+  validate(): Boolean {
+    if(super.validate()) {
+      return this.name?.length > 0
+    }
+    return false;
+  }
+
+  getAcceptableStructure() {
+    return {
+      name: {
+        required: true,
+        type: 'string'
+      }
+    }
+  }
 }
